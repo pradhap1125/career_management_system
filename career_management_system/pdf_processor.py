32261f9d1ec6455b980f3ebac288a3f2e5b120af
pradhap1125 <pradhapreee@gmail.com>
2025-04-29 18:04:17 -0400

    changes for attachment upload

--- career_management_system/pdf_processor.py
+++ career_management_system/pdf_processor.py
@@ -2,6 +2,7 @@ import os
 
 from flask import jsonify
 import faiss
+import numpy as np
 from career_management_system.resume_search import load_data, model, filenames
 
 
@@ -12,15 +13,15 @@ def process_pdf(file):
     for text in texts:
         filenames.append(file.filename)
     index = faiss.read_index("resume_index.faiss")
-    embeddings = model.encode(texts, convert_to_numpy=True)
-    faiss.normalize_L2(embeddings)
+    embeddings = model.encode(texts, normalize_embeddings=True)
+    embeddings = np.array(embeddings).astype('float32')
     index.add(embeddings)
     faiss.write_index(index, "resume_index.faiss")
     return jsonify(message="File uploaded!")
 
-def delete_pdf(file):
+def delete_pdf(fileName):
     resume_dir = "D:\\resume_test\\"
-    file_path = resume_dir + file.filename
+    file_path = resume_dir + fileName
     if os.path.exists(file_path):
         os.remove(file_path)
         return jsonify(message="File deleted!")
